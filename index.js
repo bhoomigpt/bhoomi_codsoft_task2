e7c475ec423d6cf6cfe2d0a68e0789913f79162a
bhoomigpt <118710274+bhoomigpt@users.noreply.github.com>
2023-09-18 21:34:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,42 @@
-let name =
-                document.getElementById("nameInput").value;
-            let email =
-                document.getElementById("emailInput").value;
-            let mobile =
-                document.getElementById("numberInput").value;
-            let address =
-                document.getElementById("addressInput").value;
-            
-            // Get the table and insert a new row at the end
-            let table = document.getElementById("outputTable");
-            let newRow = table.insertRow(table.rows.length);
-            
-            // Insert data into cells of the new row
-            newRow.insertCell(0).innerHTML = name;
-            newRow.insertCell(1).innerHTML = email;
-            newRow.insertCell(2).innerHTML = mobile;
-            newRow.insertCell(3).innerHTML = address;
-            newRow.insertCell(4).innerHTML =
-                '<button onclick="editData(this)">Edit</button>'+
-                '<button onclick="deleteData(this)">Delete</button>';
-            
-            
-        }
-  
-        function editData(button) {
-            
-            
-            let nameCell = row.cells[0];
-            let emailCell = row.cells[1];
-            let mobileCell = row.cells[2];
-            let addressCell = row.cells[3];
-            
-            /
-            nameCell.innerHTML = nameInput;
-            emailCell.innerHTML = emailInput;
-            mobileCell.innerHTML = numberInput;
-            addressCell.innerHTML = addressInput;
-        }
-        function deleteData(button) {
-            
-            
-        }
-        function clearInputs() {
-            
-            
-            document.getElementById("nameInput").value = "";
-            document.getElementById("emailInput").value = "";
-            document.getElementById("numberInput").value = "";
-            document.getElementById("addressInput").value = "";
-        }
+let itemsArray=[ ]
+const screen = document.getElementById("screen")
+const ulEl = document.getElementById("items") 
+
+const itemsFromLocalStorage = JSON.parse( localStorage.getItem("listItems"))
+
+if (itemsFromLocalStorage) {
+    itemsArray = itemsFromLocalStorage
+    load()
+}
+
+function addItems(){
+    itemsArray.push(screen.value)
+    screen.value = ""
+    localStorage.setItem("items", JSON.stringify(itemsArray))
+    load()
+}
+
+function load() {
+    let listItems = " "
+    for ( let i=0 ; i < itemsArray.length ; i++){
+        listItems += `
+        <div class="container">
+            <li> ${itemsArray[i]} </li>
+            <button value="${i}" onclick="deleteItem('${i}')"> Delete </button>
+        </div>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
+
+function allDelete(){
+    itemsArray = []
+    localStorage.clear()
+    load()
+}
+
+function deleteItem(value){
+    itemsArray.splice(value,1)
+    localStorage.setItem("items", JSON.stringify(itemsArray))
+    load()
+}
